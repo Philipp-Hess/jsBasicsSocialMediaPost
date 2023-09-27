cb8fa7b54fa37c81b4c03456a690cff56678d89f
Philipp-Hess <145116105+Philipp-Hess@users.noreply.github.com>
2023-09-27 13:12:39 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const title = "a title";
+const text = "text";
+const numberOfLikes = 0;
+const theUserWhoCreatedThePost = "username";
+const isReported = true;
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+
+console.log(title);
+console.log(text);
+console.log(numberOfLikes);
+console.log(theUserWhoCreatedThePost);
+console.log(isReported);
+
+const add = numberOfLikes + 1;
+
+console.log(add);
+
+// --^-- write your code here --^--
